1851e861b51bc561fedafd09d6203286fcd2f8f2
Nico Van Laerebeke <nvanlaerebeke@users.noreply.github.com>
2019-04-23 20:52:44 +0200

    Add it

---
+++ src/CommandLineMenu/RequestInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleManager {
+	public class RequestInput {
+		private readonly string Message;
+		public RequestInput(string pRequestMessage = "") {
+			Message = pRequestMessage;
+			if(String.IsNullOrEmpty(pRequestMessage)) {
+				Message = "Input";
+			}
+		}
+
+		public string Start() {
+			Console.Write($"{Message} => ");
+			return Console.ReadLine();
+		}
+	}
+}
